5f332de03a7dd6782f780caca7fa9ea585fb3627
xaco04 <xavivico04@gmail.com>
2023-07-11 18:08:57 +0200

    importacio de la versio 0.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ListapersComponent } from './home/listapers/listapers.component';
+import { PersComponent } from './home/listapers/pers/pers.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { AboutComponent } from './home/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    ListapersComponent,
+    PersComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
